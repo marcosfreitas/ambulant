36629c9b8d45bfc900fdfa28dde88cd6827c5ecd
Marcos Freitas <marcos@okn.com.br>
2020-04-27 20:39:53 -0300

    :construction: refactor of app.js to modularize configs and use class structure

--- src/app.js
+++ src/app.js
@@ -1,1 +1,30 @@
-const app = require('./config/server')();
+const express = require('express')
+const dotenv = require('dotenv')
+
+class App {
+    constructor() {
+       /**
+        * Preparing required modules
+        */
+        this.express = express();
+        this.envFile();
+
+        /**
+         * APP DEBUG
+         * define a global CONST checked in services classes
+         */
+        this.IS_DEBUG = process.env.APP_DEBUG;
+
+        this.middlewares();
+        this.routes();
+    }
+
+    /**
+     * Prepare dotenv to use production or development .env file
+     */
+    envFile() {
+        this.dotenv = dotenv.config({
+            path: process.env.NODE_ENV === 'dev' ? '.env.dev' : '.env'
+        });
+    }
+}
